3005f1b9524142b1d9d75a6154960c6097d4cf38
Limarev Ivan <ivantaketo@gmail.com>
2024-06-11 12:59:52 +0600

    Add Modal component in App.tsx, install bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import {useState} from "react";
+import Modal from "./components/Modal/Modal";
 
 const App = () => {
+  const [showModal, setShowModal] = useState<boolean>(true);
+
     return (
         <div className="App">
-
-        </div>
+          <Modal show={showModal} title="Some kinda modal title" onClose={() => setShowModal(false)} >
+            <div className="modal-body">
+              <p>This is modal content</p>
+            </div>
+          </Modal>
+      </div>
     );
 };
 
